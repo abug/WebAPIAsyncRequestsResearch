67043a7c99cce64726a28c45802e9b01a3b63a8f
Anton Bugai <abu@smart-it.com>
2017-12-21 14:23:30 +0200

    Add project files.

---
+++ WebAPIAsyncRequestsResearch/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPIAsyncRequestsResearch.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebAPIAsyncRequestsResearch/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPIAsyncRequestsResearch.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
